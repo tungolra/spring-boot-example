61e6409fb2c207b435eba415cbe5ded211049fe3
rtungol <rarrtungol@gmail.com>
2023-02-13 22:33:56 -0500

    update to readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # spring-boot-example
+
+### About
+First Java Springboot application creating full CRUD functionality using PostgreSQL database and held in Docker
+
+### Technologies 
+- Java
+- PostgreSQL
+- Springboot 
+- Docker
--- src/main/java/com/ralphcode/Main.java
+++ src/main/java/com/ralphcode/Main.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,4 +53,11 @@ public class Main {
     public void deleteCustomer(@PathVariable("customerId") Integer id){ 
         customerRepository.deleteById(id);
     }
+
+    //edit existing customer 
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id ){ 
+        customerRepository.
+    }
+
 }
